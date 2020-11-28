2972b9e66de12df49d411697553726aeebe9b036
Anvar <fort2652@rambler.ru>
2020-11-28 18:50:25 +0300

    verstka done

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ let button = document.querySelector('.more-info');
 button.onclick = function () {
     alert('More-info');
 };
+
+let buttonmessage = document.querySelector('.send-message');
+buttonmessage.onclick = function(){
+    alert ('Send message');
+};
