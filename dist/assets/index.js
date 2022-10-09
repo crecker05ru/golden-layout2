614e804aff20459762ba94c4b1e6a81521f6b6c1
Anvar <fort2652@rambler.ru>
2022-10-09 19:51:55 +0300

    build

---
+++ dist/assets/index.js
@@ -0,0 +1,10 @@
+let moreBtn = document.querySelector('.more-info');
+
+moreBtn.onclick = function () {
+    alert('More-info Maga');
+};
+
+let msgBtn = document.querySelector('.send-message');
+msgBtn.onclick = function(){
+    alert ('Send message');
+};
